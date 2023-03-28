e4f657d0b422e9ec7eee9f3ea7450fbd271d9a3b
jacksonarrey <agborarrey93@gmail.com>
2023-03-28 15:42:14 +0000

    0x0A. Configuration management

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,11 @@
+# kill process killmenow
+
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
+}# kill process killmenow
+
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
+}
