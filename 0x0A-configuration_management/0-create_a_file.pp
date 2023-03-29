ec4e31ec71bc433ea4927078461e86c54f06eb51
jacksonarrey <agborarrey93@gmail.com>
2023-03-29 07:59:25 +0000

    0x0A. Configuration management adjustments

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -5,11 +5,4 @@ file { '/tmp/school':
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-}# creates a file in /tmp
-
-file { '/tmp/school':
-  content =>'I love Puppet',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -3,9 +3,4 @@
 exec { 'pkill':
   command  => 'pkill killmenow',
   provider => 'shell',
-}# kill process killmenow
-
-exec { 'pkill':
-  command  => 'pkill killmenow',
-  provider => 'shell',
 }
