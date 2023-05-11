240820656fa61f1623933d012ec5111fbed5fcf2
root <agborarrey93@gmail.com>
2023-05-11 01:48:22 -0700

    0x15-api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,18 +1,42 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to CSV format."""
+"""
+This module gathers data from a REST API to retrieve information
+about the TODO list progress for a given employee ID.
+"""
+
 import csv
+import json
 import requests
 import sys
 
-if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
-         ) for t in todos]
+if __name__ == "__main__":
+    """
+    Retrieves the TODO list progress for a given employee ID
+    and exports the data to a CSV file.
+    """
+    # Get employee information
+    user_id = int(sys.argv[1])
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+    user_response = requests.get(user_url).json()
+    username = user_response.get("username")
+
+    # Get tasks information
+    todo_url = "https://jsonplaceholder.typicode.com/todos"
+    todos_response = requests.get(todo_url).json()
+
+    # Filter tasks owned by the employee
+    tasks = [
+        [user_id, username, task.get("completed"), task.get("title")]
+        for task in todos_response
+        if task.get("userId") == user_id
+    ]
+
+    # Export data to CSV file
+    filename = "{}.csv".format(user_id)
+    with open(filename, "w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow([
+            "USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"
+        ])
+        writer.writerows(tasks)
