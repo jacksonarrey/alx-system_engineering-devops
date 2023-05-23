657912793d33462e2c8756cb77c0187f86c7a6ba
root <agborarrey93@gmail.com>
2023-05-23 03:16:15 -0700

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
-"""A function that queries the Reddit API"""
-
+"""
+    Task 0
+"""
 import requests
-def number_of_subscribers(subreddit):
-    url = "https://www.reddit.com/r/{}/about".format(subreddit)
-    headers = {"user.Agent": "MyApp/1.0"}
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200
-        data = response.json()
-        count = data.get('data').get('subscribers')
-        return count
-    return 0
+
+def number_of_subscribers(subreddit):
+    """
+        Returns the number of subscribers for a given subreddit.
+        Returns 0 if invalid subreddit was given
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    """https://stackoverflow.com/questions/10606133/"""
+    headers = {
+            "User-Agent": "redditdev scraper by u/coderboy-exe",
+            "From": "coderboy.exe@gmail.com"
+    }
+
+    res = requests.get(url, headers=headers)
+    if res.status_code == 200:
+        data = res.json()
+        return data.get("data").get("subscribers")
+    else:
+        return 0
